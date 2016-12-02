10230944085b54db3f438c32f45513664487085d
Gokul Krishna <gkrishna3@sapient.com>
2016-12-02 23:36:24 +0530

    Refactor - Implemented Function Logic

--- src/java/com/dev/NumbersIntoWords.java
+++ src/java/com/dev/NumbersIntoWords.java
@@ -1,10 +1,17 @@
 package java.com.dev;
 
 public class NumbersIntoWords {
-	
-	public String convertNumIntoWords(int number)
-	{
-		return "one";
+
+	public String convertNumIntoWords(int number) {
+		String numToWord = "";
+		for (WordToNum num : WordToNum.values()) {
+			if (number == num.getNumber()) {
+				numToWord = num.name();
+
+			}
+
+		}
+		return numToWord;
 	}
 
 }
