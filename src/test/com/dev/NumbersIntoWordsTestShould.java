ecd511f93956f845eb68ac8185969f90daff6aaa
Gokul Krishna <gkrishna3@sapient.com>
2016-12-02 23:38:56 +0530

    Green - Test for positive test cases

--- src/test/com/dev/NumbersIntoWordsTestShould.java
+++ src/test/com/dev/NumbersIntoWordsTestShould.java
@@ -11,6 +11,12 @@ public class NumbersIntoWordsTestShould extends TestCase {
 	@Test
 	public void return_1_into_Words() {
 		NumbersIntoWords numIntoWords = new NumbersIntoWords();
-		assertEquals("zero", numIntoWords.convertNumIntoWords(0));
+		assertEquals("One", numIntoWords.convertNumIntoWords(1));
+	}
+	
+	@Test
+	public void return_2_into_Words() {
+		NumbersIntoWords numIntoWords = new NumbersIntoWords();
+		assertEquals("Two", numIntoWords.convertNumIntoWords(2));
 	}
 }
