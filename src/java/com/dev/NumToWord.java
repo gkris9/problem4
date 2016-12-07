e59ba09395a30cd88c8481faa5af5b001974503a
Gokul Krishna <gkrishna3@sapient.com>
2016-12-07 15:19:34 +0530

    REFACTOR - Modified code updated the range between 1 to 999

---
+++ src/java/com/dev/NumToWord.java
@@ -0,0 +1,11 @@
+package java.com.dev;
+
+public class NumToWord {
+
+	String[] tensNames = { "", " ten", " twenty", " thirty", " forty", " fifty", " sixty", " seventy", " eighty",
+			" ninety" };
+
+	String[] numNames = { "", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine", " ten",
+			" eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen", " eighteen",
+			" nineteen" };
+}
--- src/java/com/dev/NumbersIntoWords.java
+++ src/java/com/dev/NumbersIntoWords.java
@@ -1,17 +1,33 @@
 package java.com.dev;
 
-public class NumbersIntoWords {
+import java.util.Arrays;
+import java.util.HashMap;
 
+public class NumbersIntoWords extends NumToWord {
 	public String convertNumIntoWords(int number) {
-		String numToWord = "";
-		for (WordToNum num : WordToNum.values()) {
-			if (number == num.getNumber()) {
-				numToWord = num.name();
+		String current;
+		String numWords;
+		int num_new = number;
+		if (number % 100 < 20) {
+			current = numNames[number % 100];
+			number /= 100;
 
-			}
+		} else {
+			current = numNames[number % 10];
+			System.out.println("current" + current);
+			number /= 10;
 
+			current = tensNames[number % 10] + current;
+			number /= 10;
 		}
-		return numToWord;
+		if (num_new > 100) {
+			numWords = numNames[number] + " hundred" + current;
+		} else {
+
+			numWords = numNames[number] + current;
+		}
+
+		return numWords;
 	}
 
 }
